ea33e25c23bf00f676dc99cb61a0be48343deffa
PSYclownYT <116015847+PSYclownYT@users.noreply.github.com>
2025-03-15 10:25:35 -0400

    epicest chanves

--- source/Scenes/win.gd
+++ source/Scenes/win.gd
@@ -1,10 +1,10 @@
-extends CSGCylinder3D
+extends CSGBox3D
 
 #class name
-class_name Jumppad
+class_name Winpad
 
 #value variables
-@export_group("value variables")
+@export_group("Scene")
 @export var scene : Resource
 
 func _on_area_3d_area_entered(area):
